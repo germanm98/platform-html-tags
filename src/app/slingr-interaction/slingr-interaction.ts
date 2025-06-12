df27079c6ce3a4e65322c130944708fcaab4bb13
germanm98 <germanmanzur98@gmail.com>
2025-06-12 14:56:08 -0300

    adds slingr-interaction tag

---
+++ src/app/slingr-interaction/slingr-interaction.ts
@@ -0,0 +1,107 @@
+import { html, LitElement, css } from "lit";
+import { customElement, property } from "lit/decorators.js";
+import "../slingr-button/slingr-button";
+import "../slingr-icon/slingr-icon";
+
+@customElement("slingr-interaction")
+export class SlingrInteraction extends LitElement {
+  static styles = css`
+    a {
+      cursor: pointer;
+      color: var(--link-color, #00a3e0);
+    }
+
+    a:hover,
+    a:focus {
+      color: var(--link-hover-color, #006b93);
+    }
+  `;
+
+  @property()
+  label: string = "";
+
+  @property()
+  icon: string = "";
+
+  @property()
+  entity: string = "";
+
+  @property()
+  interaction: string = "";
+
+  @property({ type: Object })
+  defaultParams: { [key in string]: string } = {};
+
+  @property({attribute: "record-id"})
+  recordId: string = "";
+
+  @property({
+    attribute: "record-ids",
+    type: Array,
+    converter: (recordIds: string | null) => {
+      let ids: Array<string> = [];
+      if (typeof recordIds === "string") {
+        ids = recordIds.split(",");
+      }
+      return ids;
+    },
+  })
+  recordIds: Array<string> = [];
+
+  @property({ attribute: "display-type" })
+  displayType: "button" | "link" = "button";
+
+  @property({ attribute: "button-style" })
+  buttonStyle: string = "default";
+
+  manageClick() {
+    let myEvent = new CustomEvent("slingr-tag-interaction", {
+      detail: {
+        entity: this.entity,
+        interaction: this.interaction,
+        recordIds: this.recordId ? [this.recordId] : this.recordIds,
+        defaultParams: this.defaultParams
+      },
+      bubbles: true,
+      composed: true,
+    });
+    this.dispatchEvent(myEvent);
+  }
+
+  renderAsButton() {
+    return html`
+      <slingr-button
+        @click=${this.manageClick}
+        icon=${this.icon}
+        label=${this.label}
+        button-style=${this.buttonStyle}
+      >
+      </slingr-button>
+    `;
+  }
+
+  renderAsLink() {
+    return html`
+      <a @click=${this.manageClick}>
+        ${this.icon ? html`<slingr-icon name=${this.icon}></slingr-icon>` : ""}
+        ${this.label || ""}
+      </a>
+    `;
+  }
+
+  showError() {
+    return html`interaction label must be provided`;
+  }
+
+  protected render() {
+    return this.label
+      ? this.displayType === "link"
+        ? this.renderAsLink()
+        : this.renderAsButton()
+      : this.showError();
+  }
+
+  protected createRenderRoot() {
+    return this;
+  }
+}
